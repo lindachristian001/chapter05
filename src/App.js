811b4386c91b7fe5cbe6e85cba5f9528365b455c
lindachristian001 <lindachristian90@gmail.com>
2022-09-05 14:58:54 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { Suspense, lazy } from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Header from './components/Header/Header';
+import AppContext from './context/AppContext';
+
+const Lists = lazy(() => import('./pages/Lists'));
+const ListDetail = lazy(() => import('./pages/ListDetail'));
+const ListForm = lazy(() => import('./pages/ListForm'));
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
+
+const AppWrapper = styled.div`
+  text-align: center;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <AppWrapper>
+        <BrowserRouter>
+          <Header />
+          <Suspense fallback={<div>Loading...</div>}>
+            <AppContext>
+              <Routes>
+                <Route path='/' element={<Lists />} />
+                <Route path='/list/:listId/new' element={<ListForm />} />
+                <Route path='/list/:listId' element={<ListDetail />} />
+              </Routes>
+            </AppContext>
+          </Suspense>
+        </BrowserRouter>
+      </AppWrapper>
+    </>
   );
 }
-
 export default App;
+
